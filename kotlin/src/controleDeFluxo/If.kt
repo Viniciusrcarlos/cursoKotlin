db40d315d44169fbf88a1a0823271420d51681b5
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-01-06 15:18:29 -0300

    fluxo

--- kotlin/src/controleDeFluxo/If.kt
+++ kotlin/src/controleDeFluxo/If.kt
@@ -6,13 +6,20 @@ fun maiorDeIdade(idade: Int) {
         if (idade >= 60) {
             println("Terceira idade")
         }
-    }else if (idade <= 10){
+    } else if (idade <= 10) {
         println("Criança")
     } else {
         println("Menor de idade")
     }
 }
 
+fun saudacoes(dia: Boolean) = if (dia) {
+    "Bom dia!"
+} else {
+    "Boa noite!"
+}
+
 fun main() {
     maiorDeIdade(8)
+    println(saudacoes(false))
 }
---
+++ kotlin/src/controleDeFluxo/PraticaIfElse.kt
@@ -0,0 +1,27 @@
+package controleDeFluxo
+
+fun bonus(cargo: String, tempoDeExperiencia: Int) {
+    if (cargo == "gerente") {
+        if (tempoDeExperiencia < 2) {
+            println("Bonus de R$2.000,00")
+        } else {
+            println("Bonus de R$3.000,00 por ter mais de 2 anos de experiencia.")
+        }
+    } else if (cargo == "coordenador") {
+        if (tempoDeExperiencia < 1) {
+            println("Bonus de R$1.500,00")
+        } else {
+            println("Bonus de R$1.800,00 por ter mais de 1 ano de experiencia.")
+        }
+    } else if (cargo == "engenheiro") {
+        println("Bonus de R$1.000,00")
+    } else if (cargo == "estagiario") {
+        println("Bonus de R$500,00")
+    } else {
+        println("O Cargo $cargo não foi encontrado.")
+    }
+}
+
+fun main() {
+    bonus("coordenador", 1)
+}
