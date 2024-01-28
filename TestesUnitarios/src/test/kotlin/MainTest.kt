00909953f1c89fb30b31d79664136a6ead07c7f0
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-01-28 11:38:00 -0300

    testes

--- TestesUnitarios/src/test/kotlin/MainTest.kt
+++ TestesUnitarios/src/test/kotlin/MainTest.kt
@@ -1,14 +1,37 @@
+import org.example.abc
 import org.example.countXO
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 class MainTest {
 
     @Test
     @DisplayName("Teste metodo XXOO")
     fun testCountXO() {
-        Assertions.assertTrue(countXO("xxoo"))
+        Assertions.assertAll(
+            {Assertions.assertTrue(countXO("xxoo"))},
+            {Assertions.assertTrue(countXO("xxoo"))},
+            {Assertions.assertTrue(countXO("xxoo"))},
+            {Assertions.assertTrue(countXO("xxoooo"))}
+        )
     }
+
+    @Test
+    @Disabled
+    fun testeNaoImplementado() {
+
+    }
+
+//    @Test
+//    fun vaiFalhar() {
+//        fail("Falhou")
+//    }
+
+    @Test
+    fun exception() {
+        assertThrows<NullPointerException> { abc() }
+    }
+
+    
+
 
 }
